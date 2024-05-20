0c4fdbc339512829701f0ad59d1f162be84ec974
Sean Kirk Handayan <handayan.seankirk1@gmail.com>
2024-05-20 10:47:00 +0800

    Updates

---
+++ static/js/952.15be30c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb_frontend=self.webpackChunkweb_frontend||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.15be30c9.chunk.js.map
